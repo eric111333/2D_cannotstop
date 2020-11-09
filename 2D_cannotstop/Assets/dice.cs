be83a5de49656f8fcd81f4c2c33cc1142f0f48d6
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-11-09 13:46:15 +0800

    11

---
+++ 2D_cannotstop/Assets/dice.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class dice : MonoBehaviour
+{
+    public Image diceimage;
+    public Sprite[] dicepoint;
+
+    public void DiceRoll()
+    {
+
+        int mun = Random.Range(0, dicepoint.Length);
+        diceimage.sprite = dicepoint[mun];
+
+        print(dicepoint[mun]);
+    }
+
+}
